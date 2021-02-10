2777a0ca43266ef1b444172f7d4a31770d75b75a
ebinabo <edgarjohn95@gmail.com>
2021-02-10 11:39:22 +0200

    add more questions

--- questions.js
+++ questions.js
@@ -7,7 +7,51 @@ export const questions = {
         },
         {
             question:
-                "List out your contact details you would like to be reached on",
+                "List out contact details of the brand you would like to be displayed on the website",
+            description: "please include socials if you have any",
+        },
+        {
+            question: "Does your company have any partners currently?",
+            description:
+                "If yes, could you list them out? Are you allowed to use their logos?",
+        },
+        {
+            question:
+                "Do you have any legal documents you would like to have on your site?",
+        },
+        {
+            question:
+                "Are there current or potential job opportunities available at your company?",
+            description: "This would mean a careers page would be present",
+        },
+        {
+            question: "Do you currently have any competitors?",
+            description:
+                "If yes, please list them out. Are there any of their strengths you're interested in?",
+        },
+    ],
+    "Design and Content": [
+        {
+            question:
+                "How do you see the new websites changing in the 6 to 24 months?",
+            description:
+                "(for example - you don’t have a blog section at this moment, but you are \
+                thinking about adding a blog section in the future or adding new products or services..)",
+        },
+        {
+            question:
+                "Do you have defined brand guidelines we would need \
+                to follow and how flexible we are in its implementation?",
+            description:
+                "(in terms of colors, logo, typography, illustrations, communication)",
+        },
+        {
+            question:
+                "Do you have your own gallery of photos or any other visual \
+                materials that can be used for the site?",
+            description:
+                "(if you don’t have an applicable gallery of visual materials, are you planning \
+                to allocate a budget for photo shooting or you would like to use stock photos?)",
         },
     ],
     "Technical Requirements": [
@@ -36,6 +80,15 @@ export const questions = {
         {
             question: "What kind of payments are going to be made?",
             description: "(for example: one off payments and/or subscription)",
+        },
+    ],
+    Recommended: [
+        {
+            question:
+                "Would you like for us to maintain the site after development is completed?",
+            description:
+                "This includes hosting, security, analytics and continuous SEO strategy to increase traffic. \
+                This would be contract based and requires a retainer clause.",
         },
     ],
 }
